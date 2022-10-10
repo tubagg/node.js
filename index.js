8890ddf2aa58c52ba7e8ac02edfb6bd9f8130707
Tuba Bozkurt <tubzkrt@gmail.com>
2022-10-10 16:59:35 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import {hello,topla} from './my-module.js'
+import hello, {topla,cikar,text,user,users} from './my-module.js'
+
 console.log(topla(2,4));
+console.log(cikar(5,4));
+console.log(text);
+console.log(user);
+console.log(users);
+
 hello("tuba");
--- my-module.js
+++ my-module.js
@@ -1,7 +1,33 @@
- const hello = name => {
-    console.log("hello" + name);
+ export default (name) => {
+    console.log(`hello ${name}`);
 };
 
- const topla = (a,b) => a+b;
+  const topla = (a,b) => a+b;
+  const cikar = (a,b) => a-b;
 
- export{hello,topla};
+  const text = 'Text';
+  const user = {
+    name:'Tugba',
+    surname:'Bozkurt',
+
+ };
+
+  const users = [{
+    name:'Ayşe',
+    surname:'Yıldırım',
+    
+ },{
+    name:'Emir',
+    surname:'Seyaan',
+ },
+];
+
+export{
+    topla,
+    cikar,
+    text,
+    user,
+    users,
+};
+
+
